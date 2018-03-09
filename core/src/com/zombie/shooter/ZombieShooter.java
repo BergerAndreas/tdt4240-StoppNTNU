8d0b45bccce4ef48c0992288252af034c902aed8
haavarop <haavard@vaage.com>
2018-03-09 13:07:21 +0100

    Add GSM, AbstractScreen. Update ZombieShooter.java
    Add GameScreenManager to handle switching between game screens
    Add AbstractScrenn that acts as the screen superclass
    Updated ZombieShooter.java with global fields

--- core/src/com/zombie/shooter/ZombieShooter.java
+++ core/src/com/zombie/shooter/ZombieShooter.java
@@ -1,33 +1,57 @@
 package com.zombie.shooter;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.zombie.shooter.managers.GameScreenManager;
 
-public class ZombieShooter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class ZombieShooter extends Game {
+
+	public static String APP_TITLE = "Tødden er ei dås";
+	public static double APP_VERSION = 0.1;
+	public static int APP_DESKTOP_WIDTH = 720;
+	public static int APP_DESKTOP_HEIGHT = 420;
+	public static int APP_FPS = 60; //Cinematic?!??!
+
+
+	// Mangers
+	public AssetManager assets;
+	public GameScreenManager gsm;
+
+	public SpriteBatch batch;
+	public ShapeRenderer shapeBatch;
+
+
 	@Override
 	public void create () {
+		shapeBatch = new ShapeRenderer();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		assets = new AssetManager();
+		gsm = new GameScreenManager(this);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+			Gdx.app.exit();
+		}
 	}
 	
 	@Override
 	public void dispose () {
+		super.dispose();
 		batch.dispose();
-		img.dispose();
+		shapeBatch.dispose();
+		assets.dispose();
 	}
 }
---
+++ core/src/com/zombie/shooter/managers/GameScreenManager.java
@@ -0,0 +1,53 @@
+package com.zombie.shooter.managers;
+
+import com.badlogic.gdx.Screen;
+import com.zombie.shooter.ZombieShooter;
+import com.zombie.shooter.screens.AbstractScreen;
+
+import java.util.HashMap;
+
+/**
+ *  Created by opium on 09.03.18.
+ */
+
+public class GameScreenManager {
+
+    public final ZombieShooter app;
+    private HashMap<STATE, AbstractScreen> gameScreen;
+
+    //Add additional screens as they are added
+    public enum STATE{
+        MAIN_MENU,
+        SINGLE_PLAYER,
+        MULTI_PLAYER_LOBBY,
+        MULTI_PLAYER,
+        SETTINGS
+    }
+
+    public GameScreenManager(final ZombieShooter app){
+        this.app = app;
+
+        initGameStates();
+
+        //Set start screen here
+
+    }
+
+    private void initGameStates(){
+        this.gameScreen = new HashMap<STATE, AbstractScreen>();
+        // Add new game states here
+
+    }
+
+    public void setScreen(STATE nextScreen) {
+        app.setScreen(gameScreen.get(nextScreen));
+    }
+
+    public void dispose(){
+        for(AbstractScreen screen : gameScreen.values()) {
+            if(screen != null) {
+                screen.dispose();
+            }
+        }
+    }
+}
